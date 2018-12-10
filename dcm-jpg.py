348b99033333943f77f3f33a5b31e3cba5d72700
akshay goel <akshay.k.goel@gmail.com>
2018-12-10 18:27:29 -0500

    dicom US slices exp to jpeg

--- dcm-jpg.py
+++ dcm-jpg.py
@@ -1,21 +1,36 @@
 import numpy as np
 import png
 import pydicom
+from PIL import Image
+from time import sleep
 
-ds = pydicom.dcmread(path)
+ds = pydicom.dcmread("/home/akshay/Downloads/thyroid/data/D13.dcm")
 
 shape = ds.pixel_array.shape
 
 # Convert to float to avoid overflow or underflow losses.
-image_2d = ds.pixel_array.astype(float)
+image_3d = ds.pixel_array.astype(float)
 
 # Rescaling grey scale between 0-255
-image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+image_3d_scaled = (np.maximum(image_3d,0) / image_3d.max()) * 255.0
 
 # Convert to uint
-image_2d_scaled = np.uint8(image_2d_scaled)
+image_3d_scaled = np.uint8(image_3d_scaled)
 
-# Write the PNG file
-with open(destination, 'wb') as png_file:
-    w = png.Writer(shape[1], shape[0], greyscale=True)
-    w.write(png_file, image_2d_scaled)
+interval = 10
+
+for x in range(1,shape[0], interval):
+
+	print(x)
+	# Write the PNG file
+	image_2d_scaled = image_3d_scaled[x,:,:]
+	
+	#im = Image.fromarray(image_2d_scaled)
+	#im.save("/home/akshay/Dev/us-anonymize/outputs/image" + str(x) + ".jpeg" )
+	with open("/home/akshay/Dev/us-anonymize/outputs/image" + str(x)+".png", 'wb') as png_file:
+	    w = png.Writer(shape[2], shape[1], greyscale=True)
+	    w.write(png_file, image_2d_scaled)
+	    sleep(0.05)
+	print("written")
+
+
